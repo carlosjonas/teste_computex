743b0cd87b9a550418ddc95752186d9bddce9e3f
Carlos Jonas <jonas.fonseca@opovodigital.com>
2023-01-27 00:57:00 -0300

    refactor: incluindo os links dos includes

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <?php 
-	$url = "http://camerascomputex.ddns.net:8080/escola/mobile_login.php?matricula=2011004&senha=99999999&token=X&so=ios";
-	$jsonString = file_get_contents($url);
-	//echo "<pre>"; print_r($jsonString); echo "</pre>"; 
-	$jsoninfo = json_decode($jsonString,true);
-	//echo "<pre>"; print_r($jsoninfo); echo "</pre>";
+	
 
+	
+	include __DIR__.("/url.php");
 	include __DIR__.("/includes/header.php");
 	include __DIR__.("/includes/modal.php");
+	include __DIR__.("/includes/listagem.php");
 	include __DIR__.("/includes/footer.php");
 ?>
 
