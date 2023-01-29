253aabc97a9df012251942d6a7bad4cdb86d2dad
Carlos Jonas <jonas.fonseca@opovodigital.com>
2023-01-28 21:22:36 -0300

    refactor: mudando estrutura dos acessos

--- includes/listagem_acessos.php
+++ includes/listagem_acessos.php
@@ -6,18 +6,54 @@
 				<?php  echo "<pre>"; print_r($jsoninfoAcesso); echo "</pre>"; ?>
 				<h1 class="mt-3 text-center">Listagem de Turmas</h1>
 
-
-				<ol class="list-group list-group-numbered mt-3">
-					<?php foreach ($jsoninfoAcesso as $acesso): ?>
-						<li class="list-group-item d-flex justify-content-between align-items-start">
-						    <div class="ms-2 me-auto">
-						      <div class="fw-bold">Turma: <?= $acesso['grau_longo'];?></div>
-						      Série: <?= $acesso['serie_longa'];?>
-						    </div>
-						    <span class="badge bg-primary rounded-pill">Turno: <?= $acesso['turno'];?></span>
-						</li>
-					<?php endforeach ?>
-				</ol>
+				<table class="table">
+					
+					  <thead>
+					    <tr>
+					      <th scope="col">Série</th>
+					      <th scope="col">Turma</th>
+					      <th scope="col">Ano</th>
+					      <th scope="col">Turno</th>
+					    </tr>
+					  </thead>
+					  <tbody>
+					  	<?php foreach ($jsoninfoAcesso as $acesso): ?>
+					    	<tr>
+					    	
+						        <td>
+						        	<?= $acesso['serie_longa'];?>
+						         		
+						         </td>
+						        <td><?= $acesso['grau_longo'];?></td>
+						        <td>
+						        	<?php 
+												$acesso['ano'] = substr_replace($acesso['ano'], '.', 4, 0);
+												echo $acesso['ano'];
+									?>		
+								</td>
+						        <td>
+						        	<?php
+						        		switch ($acesso['turno']):
+										    case "M":
+										        echo "Manhã";
+										        break;
+										    case "T":
+										        echo "Tarde";
+										        break;
+										    case "N":
+										        echo "Noite";
+										        break;
+										    default:
+										        echo "Erro";
+										endswitch;
+						        	?>
+						        		
+						        </td>
+					    	</tr>
+						<?php endforeach ?>
+					  </tbody>
+				  	
+				</table>
 			</div>
 		</section>
 	</main>
