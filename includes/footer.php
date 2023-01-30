e5ccc5893cf2a9a577ae093c91f902141d18ebe4
Carlos Jonas <jonas.fonseca@opovodigital.com>
2023-01-29 23:29:38 -0300

    refactor: mudando o dompdf para jspdf

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,17 @@
 	</div>
 		<!--Fim Container Body-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.5.3/jspdf.debug.js" integrity="sha384-NaWTHo/8YCBYJ59830LTz/P4aQZK1sS0SneOgAvhsIl3zBu8r9RevNg5lHCHAuQ/" crossorigin="anonymous"></script>
+
+    <script>
+    	function gerarPdf(){
+    		var doc = new jsPDF()
+
+			doc.fromHTML('<table> <thead> <tr> <th>Matrícula</th> <th> Nome </th> </tr> </thead> <tbody> <?php foreach ($jsoninfoAluno as $aluno): ?>
+				    	<tr> <td><?= $aluno['matricula'];?></td> <td><?= $aluno['nome'];?></td>	</tr> <?php endforeach ?>
+				  </tbody> </table>', 30)
+			doc.save('Lista de alunos.pdf')
+    	}
+    </script>
   </body>
 </html>
--- includes/listagem_acessos.php
+++ includes/listagem_acessos.php
@@ -1,48 +1,4 @@
-<?php
-// Incluindo o autoload do DOM PDF
-require_once 'dompdf/autoload.inc.php';
 
-//Criando a instancia do Dom PDF.
-use Dompdf\Dompdf;
-use Dompdf\Options;
-
-//Alterando as opções para aceitar bootstrap
-$options = new Options();
-$options->set('isRemoteEnabled', TRUE);
-
-// Instanciando a classe do Dom DPF
-$dompdf = new Dompdf($options);
-
-//Criando o código HTML que será transformado em pdf
-$dompdf->loadHtml('
-    <table class="table table-danger table-striped table-bordered">
-	  <thead>
-	    <tr class="text-center">
-	      <th scope="col">Matrícula</th>
-	      <th scope="col">Nome</th>
-	    </tr>
-	  </thead>
-	  <tbody>
-	  	<?php foreach ($jsoninfoAluno as $aluno): ?>
-	    	<tr>
-		        <td><?= $aluno["matricula"];?></td>
-		        <td><?= $aluno["nome"];?></td>
-	    	</tr>
-		<?php endforeach ?>
-	  </tbody>  	
-	</table>'
-);
-
-//Define o tipo de papel de impressão e oritenação do papel 
-$dompdf->setPaper('A4', 'landscape');
-
-// Vai renderizar o HTML como PDF
-//$dompdf->render();
-
-// Saída do pdf para a renderização do navegador.
-//$dompdf->stream("listagem_alunos.pdf", array(true)); 
-
-?>
  	<main>
 		<section>
 			<div class="container mb-5">
@@ -63,17 +19,16 @@ $dompdf->setPaper('A4', 'landscape');
 				  </thead>
 				  <tbody>
 				  	<?php foreach ($jsoninfoAcesso as $acesso): ?>
-				    	<tr>
-				    	
-					        <td><?= $acesso['serie_longa'];?></td>
-					        <td><?= $acesso['grau_longo'];?></td>
-					        <td>
-					        	<?php 
+						<tr>
+							<td><?=$acesso['serie_longa'];?></td>
+							<td><?=$acesso['grau_longo'];?></td>
+							<td>
+								<?php
 									$acesso['ano'] = substr_replace($acesso['ano'], '.', 4, 0);
 									echo $acesso['ano'];
-								?>		
+								?>
 							</td>
-					        <td>
+							<td>
 					        	<?php
 					        		switch ($acesso['turno']):
 									    case "M": 
@@ -89,9 +44,8 @@ $dompdf->setPaper('A4', 'landscape');
 									        echo "Erro";
 									endswitch;
 					        	?>
-					        		
-					        </td>
-				    	</tr>
+							</td>
+						</tr>
 					<?php endforeach ?>
 				  </tbody>
 				  	
@@ -115,6 +69,9 @@ $dompdf->setPaper('A4', 'landscape');
 					<?php endforeach ?>
 				  </tbody>  	
 				</table>
+
+				<p>Clique aqui para gerar a lista de alunos: </p>
+				<button onclick="gerarPdf()">Gerar PDF</button>
 
 			</div>
 		</section>
