508a23e9af3d83b6e512005fc0756754e5dacc3a
Carlos Jonas <jonas.fonseca@opovodigital.com>
2023-01-30 22:07:52 -0300

    refactor: mudando a chamada da logo

--- includes/header.php
+++ includes/header.php
@@ -15,7 +15,7 @@
   		<!--Menu-->
 	    <nav class="navbar navbar-expand-lg bg-danger">
 				<div class="container-fluid">
-				  <a class="navbar-brand" href="index.php"><img src="public/img/logotipo.png" alt="Logo" width="45" height="45" class="d-inline-block align-text-top"></a>
+				  <a class="navbar-brand" href="index.php"><img src="../public/img/logotipo.png" alt="Logo" width="45" height="45" class="d-inline-block align-text-top"></a>
 				  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 				    <span class="navbar-toggler-icon"></span>
 				  </button>
