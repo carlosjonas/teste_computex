10e0b0ed740d28529dddee6bef18c42964524599
Carlos Jonas <jonas.fonseca@opovodigital.com>
2023-02-08 20:35:05 -0300

    feature: adicionando a pasta vendor para alterações no projeto

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '09bca5cdcfd27cfe38429b2b79a0f874bd554da8',
+        'name' => 'teste/json',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'teste/json' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '09bca5cdcfd27cfe38429b2b79a0f874bd554da8',
+            'dev_requirement' => false,
+        ),
+    ),
+);
