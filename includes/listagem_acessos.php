038554a2715a34ef9f041550185a52439d2b2fe6
Carlos Jonas <jonas.fonseca@opovodigital.com>
2023-02-12 22:32:02 -0300

    feature: mudando a estrutura da página

--- includes/listagem_acessos.php
+++ includes/listagem_acessos.php
@@ -8,68 +8,15 @@
 				<h1 class="mt-3 text-center">Listagem de Turmas</h1>
 
 				<!--Tabela de listagem de turmas-->
-				<table class="mt-5 table table-danger table-striped table-bordered minhaTabela">
-
-				  <thead>
-				    <tr class="text-center">
-				      <th scope="col">Série</th>
-				      <th scope="col">Turma</th>
-				      <th scope="col">Ano</th>
-				      <th scope="col">Turno</th>
-				    </tr>
-				  </thead>
-				  <tbody>
-				  	<?php foreach ($jsondataAcesso as $acesso): ?>
-						<tr>
-							<td><?=$acesso['serie_longa'];?></td>
-							<td><?=$acesso['grau_longo'];?></td>
-							<td>
-								<?php
-									$acesso['ano'] = substr_replace($acesso['ano'], '.', 4, 0);
-									echo $acesso['ano'];
-								?>
-							</td>
-							<td>
-					        	<?php
-					        		switch ($acesso['turno']):
-									    case "M":
-									        echo "Manhã";
-									        break;
-									    case "T":
-									        echo "Tarde";
-									        break;
-									    case "N":
-									        echo "Noite";
-									        break;
-									    default:
-									        echo "Erro";
-									endswitch;
-					        	?>
-							</td>
-						</tr>
-					<?php endforeach ?>
-				  </tbody>
+				<table class="mt-5 table table-danger table-striped table-bordered minhaTabela" id="tabela_turmas">
 
 				</table>
 
 				<h1 class="mt-3 text-center">Listagem de Alunos</h1>
 
 				<!--Tabela com listagem de alunos-->
-				<table class="mt-5 table table-danger table-striped table-bordered minhaTabela">
-				  <thead>
-				    <tr class="text-center">
-				      <th scope="col">Matrícula</th>
-				      <th scope="col">Nome</th>
-				    </tr>
-				  </thead>
-				  <tbody>
-				  	<?php foreach ($jsondataAluno as $aluno): ?>
-				    	<tr>
-					        <td><?= $aluno['matricula'];?></td>
-					        <td><?= $aluno['nome'];?></td>
-				    	</tr>
-					<?php endforeach ?>
-				  </tbody>
+				<table class="mt-5 table table-danger table-striped table-bordered minhaTabela" id="tabela_alunos">
+
 				</table>
 
 				<!--Botão que gera o PDF-->
@@ -79,3 +26,9 @@
 			</div>
 		</section>
 	</main>
+
+	<script type="text/javascript" src="../public/scripts/scriptacesso.js"></script>
+	<script type="text/javascript">
+		loadDocAcesso();
+		loadDocAluno();
+	</script>
